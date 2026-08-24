982ffe65003104c52857820aa71532a97a87f147
Cesar Augusto <168694352+anyozasad@users.noreply.github.com>
2026-08-24 18:48:18 -0500

    Conectar APIs principales de Mallqui Gym

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PersonaController;
 use App\Http\Controllers\CursoController;
+use App\Http\Controllers\MiembroController;
+use App\Http\Controllers\MembresiaController;
+use App\Http\Controllers\ClaseController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +14,8 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('/personas', PersonaController::class);
 Route::apiResource('/cursos', CursoController::class);
+
+// API de Mallqui Gym. Las migraciones se mantienen aparte y se ejecutan con Artisan.
+Route::apiResource('/miembros', MiembroController::class);
+Route::apiResource('/membresias', MembresiaController::class);
+Route::apiResource('/clases', ClaseController::class);
