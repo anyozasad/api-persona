b12d314fc4a4c6162ca13ba68d89e150b0e86e6a
Cesar Augusto <168694352+anyozasad@users.noreply.github.com>
2026-08-25 15:13:39 -0500

    Agregar ruta de login

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing.component';
+import { LoginComponent } from './login.component';
 import { UsuarioComponent } from './usuario.component';
 import { AdminComponent } from './admin.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'usuario', component: UsuarioComponent },
   { path: 'admin', component: AdminComponent },
   { path: '**', redirectTo: '' }
