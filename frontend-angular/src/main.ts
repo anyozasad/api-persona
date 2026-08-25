4015f726ed795789b7e2e39dafc3726bd7e2b003
Cesar Augusto <168694352+anyozasad@users.noreply.github.com>
2026-08-25 15:39:03 -0500

    Activar minimizador del sidebar administrador

--- frontend-angular/src/main.ts
+++ frontend-angular/src/main.ts
@@ -1,4 +1,5 @@
 import 'zone.js';
+import './admin-sidebar-toggle';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
