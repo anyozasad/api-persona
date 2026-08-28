90a469f4665a10b013dff7f64d536621d29f25d1
Cesar Augusto <168694352+anyozasad@users.noreply.github.com>
2026-08-28 18:06:04 +0200

    Agregar modelo DetalleRutina segun diagrama

---
+++ app/Models/DetalleRutina.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetalleRutina extends Model
+{
+    protected $table = 'detalle_rutina';
+    protected $primaryKey = 'id_detalle_rutina';
+    public $timestamps = false;
+    protected $guarded = [];
+
+    public function rutina() { return $this->belongsTo(Rutina::class, 'id_rutina', 'id_rutina'); }
+}
