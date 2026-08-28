e24c8dff42c5ce0d9f07f8d00c4a4877cabd8d42
Cesar Augusto <168694352+anyozasad@users.noreply.github.com>
2026-08-28 22:19:05 +0200

    Centralizar cambios de membresia en proceso de pago

--- app/Http/Controllers/ClienteMembresiaController.php
+++ app/Http/Controllers/ClienteMembresiaController.php
@@ -1,4 +1,25 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Models\ClienteMembresia;
-class ClienteMembresiaController extends CrudController { protected string $modelClass = ClienteMembresia::class; }
+
+class ClienteMembresiaController extends Controller
+{
+    public function index()
+    {
+        return response()->json(
+            ClienteMembresia::with(['cliente', 'membresia', 'pagos'])
+                ->orderByDesc('fecha_fin')
+                ->get()
+        );
+    }
+
+    public function show(string $id)
+    {
+        return response()->json(
+            ClienteMembresia::with(['cliente', 'membresia', 'pagos'])
+                ->findOrFail($id)
+        );
+    }
+}
