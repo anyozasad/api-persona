88bebc81e632fa10a7b42d3b28cdd71e844d1b4a
Cesar Augusto <168694352+anyozasad@users.noreply.github.com>
2026-08-28 22:23:49 +0200

    Relacionar clases con entrenador y reservas

--- app/Models/Clase.php
+++ app/Models/Clase.php
@@ -7,9 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Clase extends Model
 {
     protected $table = 'clases';
+    protected $primaryKey = 'id_clase';
+    public $timestamps = false;
 
     protected $fillable = [
-        'nombre', 'descripcion', 'hora_inicio', 'hora_fin',
-        'cupo_maximo', 'estado', 'entrenador_id'
+        'id_entrenador', 'nombre', 'descripcion', 'dia_semana',
+        'hora_inicio', 'hora_fin', 'cupo_maximo', 'estado'
     ];
+
+    public function entrenador()
+    {
+        return $this->belongsTo(Entrenador::class, 'id_entrenador', 'id_entrenador');
+    }
+
+    public function reservas()
+    {
+        return $this->hasMany(Reserva::class, 'id_clase', 'id_clase');
+    }
 }
