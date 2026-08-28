ebeb0d841a047134fd77f7d1414f1d18bb59e43f
Cesar Augusto <168694352+anyozasad@users.noreply.github.com>
2026-08-28 18:08:14 +0200

    Agregar ClienteMembresiaController

---
+++ app/Http/Controllers/ClienteMembresiaController.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\ClienteMembresia;
+class ClienteMembresiaController extends CrudController { protected string $modelClass = ClienteMembresia::class; }
