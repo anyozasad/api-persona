e8618816e750e47f4c1c3fe491d15194159a27f1
Cesar Augusto <168694352+anyozasad@users.noreply.github.com>
2026-08-28 22:18:57 +0200

    Proteger historial de pagos de membresia

--- app/Http/Controllers/PagoMembresiaController.php
+++ app/Http/Controllers/PagoMembresiaController.php
@@ -1,4 +1,25 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Models\PagoMembresia;
-class PagoMembresiaController extends CrudController { protected string $modelClass = PagoMembresia::class; }
+
+class PagoMembresiaController extends Controller
+{
+    public function index()
+    {
+        return response()->json(
+            PagoMembresia::with(['clienteMembresia.cliente', 'clienteMembresia.membresia'])
+                ->orderByDesc('fecha_pago')
+                ->get()
+        );
+    }
+
+    public function show(string $id)
+    {
+        return response()->json(
+            PagoMembresia::with(['clienteMembresia.cliente', 'clienteMembresia.membresia'])
+                ->findOrFail($id)
+        );
+    }
+}
