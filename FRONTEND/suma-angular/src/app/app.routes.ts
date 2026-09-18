3fe624939efeaaab7a664adf52145720e2e4359e
Cesar Augusto <168694352+anyozasad@users.noreply.github.com>
2026-09-18 17:25:11 +0200

    Agregar ruta de membresías

--- FRONTEND/suma-angular/src/app/app.routes.ts
+++ FRONTEND/suma-angular/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { UsuarioComponent } from './pages/usuario/usuario.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { AdminIntegradoComponent } from './pages/admin/admin-integrado.component';
 import { ProductosComponent } from './pages/productos/productos';
+import { Membresias } from './pages/membresias/membresias';
 import { authGuard, logoutOnLoginGuard, roleGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
@@ -18,5 +19,6 @@ export const routes: Routes = [
   { path: 'admin', component: AdminIntegradoComponent, canActivate: [authGuard, roleGuard(['Administrador'])] },
   { path: 'admin-diseno', component: AdminComponent, canActivate: [authGuard, roleGuard(['Administrador'])] },
   { path: 'productos', component: ProductosComponent, canActivate: [authGuard, roleGuard(['Administrador'])] },
+  { path: 'membresias', component: Membresias, canActivate: [authGuard, roleGuard(['Administrador'])] },
   { path: '**', redirectTo: '' }
 ];
